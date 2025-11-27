f38f778586f5765e1aba4f354124e70f570d3aed
Jacob Aniyan <jacobaniyan@Jacobs-MacBook-Pro.local>
2025-11-27 14:48:56 +0000

    Created Topics table with columns and constraints

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+## Setting up Environment files
+
+- Set up .env files: Create .env.test and .env.development files in same directory as .gitignore.
+- In each file, set PGDATABASE as respective database names:
+
+```
+PGDATABASE = nc_news_test       // in .env.test
+```
+
+```
+PGDATABASE = nc_news           // in .env.development
+```
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,21 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(
+      `DROP TABLE IF EXISTS Comments;
+       DROP TABLE IF EXISTS Articles;
+       DROP TABLE IF EXISTS Users;
+       DROP TABLE IF EXISTS Topics; `
+    )
+    .then(() => {
+      return db.query(
+        `CREATE TABLE Topics (
+       slug VARCHAR(100) PRIMARY KEY,
+       description VARCHAR(100) NOT NULL,
+       img_url VARCHAR(1000)); `
+      );
+    }); //<< write your first query in here.
 };
 module.exports = seed;
